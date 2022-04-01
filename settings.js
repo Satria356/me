03852ba9f290a88bae787e0b80211006ca8d042b
Claraa939 <102665310+Claraa939@users.noreply.github.com>
2022-04-01 07:49:11 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -18,9 +18,9 @@ global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "Clara-Md" //namabot kalian
+global.botname = "ClaraBot-Md" //namabot kalian
 global.ownername= "ClaraKawaii" //nama kalian
-global.myweb ="https://claraKawai.com" //bebas asal jan hapus
+global.myweb ="https://api-alphabot.herokuapp.com/clara/kawai" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
 global.github = "https://zeeone-ofc.github.io/" //bebas
 global.email = "clarakawaiii5@gmail.com" //bebas
@@ -30,9 +30,9 @@ global.ownernomerr = "+6283811034750" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62 889-7141-9677","6283811034750","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.packname = 'Crated By Clara' //sticker wm ubah
-global.author = '©ClaraOfc' //sticker wm ganti nama kalian
+global.owner = ["6288971419677","6283811034750","6285342106390"] //ganti agar fitur owner bisa di gunakan
+global.packname = '©Created By' //sticker wm ubah
+global.author = 'ClaraKawaii' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
